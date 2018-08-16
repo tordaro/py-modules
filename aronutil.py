05331aae0257c995688f23951292941aabce2e82
Tord Aronsen <tordaro@gmail.com>
2018-08-16 16:24:49 +0200

    Add function to read from .key-file

--- aronutil.py
+++ aronutil.py
@@ -204,3 +204,20 @@ def direction(degrees, numeric=True):
         if intervals[i] <= degrees < intervals[i+1]:
             return interval_name[i+1]
     return interval_name[0]
+
+    def read_key(key_path):
+    with open(key_path, "r") as file:
+        header = ['ID', 'm_water', 'm_air', 'boyancy', 'L']
+        lines = {name: [] for name in header}
+        for line in file:
+            if 'Component' in line:
+                data = line.split()
+                if len(data) == 6:
+                    lines[header[0]].append(int(data[1]))
+                    lines[header[-1]].append(float(data[-1]))
+                    for i in range(1, len(header)-1):
+                        lines[header[i]].append(float(data[i+1])/g)
+        
+        df_key = pd.DataFrame(lines)
+        df_key.set_index('ID', inplace=True)
+        return df_key
